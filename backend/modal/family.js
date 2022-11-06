46239281de7291cc57fc9dde5501203fcbf4abfd
shivam.spirit <40209471+shivamsoni00@users.noreply.github.com>
2022-11-06 11:09:41 +0530

    adding mongosse sections

---
+++ backend/modal/family.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+  const familySchema = new mongoose.Schema({
+    date: Date,
+    username: String,
+    spouse: String,
+    locations: String,
+    birthYear: String,
+    presentAddress: String,
+    familypic: {
+        data: Buffer,
+        contentType: String
+    }
+  })
+  
+  familySchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Family', familySchema)
